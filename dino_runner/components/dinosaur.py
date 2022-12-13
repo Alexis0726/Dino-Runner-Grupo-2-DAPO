cfc03604467d3846fbef8aca1cd14562d36d4705
Alexis2607 <113487341+Alexis2607@users.noreply.github.com>
2022-12-13 04:24:08 -0500

    Trabajo
    Se implemento el metodo  duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -3,9 +3,11 @@ from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import RUNNING 
 from dino_runner.utils.constants import JUMPING
+from dino_runner.utils.constants import DUCKING
 
 JUMPING_ACTION = "jumping"
 RUNNING_ACTION = "running"
+DUCKING_ACTION = "dunking"
 
 class Dinosaur(Sprite):
     Y_POS = 310
@@ -20,25 +22,30 @@ class Dinosaur(Sprite):
         self.jump_velocity = self.JUMP_VELOCITY   
         self.step = 0
 
-        self.action = "running"
+        self.action = RUNNING_ACTION
 
     def update(self, user_input):
-        if self.action == "running":
+        if self.action == RUNNING_ACTION:
             self.run()
         elif self.action == JUMPING_ACTION:
             self.jump()
-
-        if self.action != JUMPING_ACTION:
-            if user_input[pygame.K_UP]:
-                self.action = JUMPING_ACTION
-            else:
-                self.action = RUNNING_ACTION
-
+        elif self.action == DUCKING_ACTION:
+            self.duck()
+   
+        if user_input[pygame.K_UP]:
+            self.action = JUMPING_ACTION
+        
+        if user_input[pygame.K_DOWN]:
+            self.action = DUCKING_ACTION
+      
         if self.step >= 9:
             self.step = 0
 
     def run(self):
         self.image = RUNNING[0] if self.step < 5 else RUNNING[1]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
         self.step += 1
 
     def jump(self):
@@ -50,6 +57,13 @@ class Dinosaur(Sprite):
             self.jump_velocity = self.JUMP_VELOCITY
             self.action = RUNNING_ACTION
     
+    def duck(self):
+        self.image = DUCKING[0] if self.step < 5 else DUCKING[1]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS + 35
+        self.action = RUNNING_ACTION
+        self.step += 1
 
     def draw(self,screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
